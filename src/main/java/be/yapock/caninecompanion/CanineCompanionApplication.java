c2816d94d7e3fa78283924389f1812922a0eee29
Beukens <beukensgaetan@gmail.com>
2024-02-12 09:16:00 +0100

    commit init

---
+++ src/main/java/be/yapock/caninecompanion/CanineCompanionApplication.java
@@ -0,0 +1,13 @@
+package be.yapock.caninecompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanineCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanineCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/yapock/caninecompanion/CanineCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yapock.caninecompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanineCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
